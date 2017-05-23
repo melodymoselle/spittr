528d7aa6d204a91e05d16c41da3bb8736a5aa39b
Melmo <melodymoselle@gmail.com>
2017-05-22 22:06:27 -0700

    Added config files. SpittrWebAppInitializer, WebConfig, RootConfig.

---
+++ src/main/java/com/melodymoselle/spittr/config/RootConfig.java
@@ -0,0 +1,15 @@
+package com.melodymoselle.spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"com.melodymoselle.spittr"},
+        excludeFilters={
+                @Filter(type= FilterType.ANNOTATION, value=EnableWebMvc.class)
+        })
+public class RootConfig {
+}
