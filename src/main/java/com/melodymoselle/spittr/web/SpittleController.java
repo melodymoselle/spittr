2cd8acfb602a3ed473992f73c0f929b7fabdce60
Melmo <melodymoselle@gmail.com>
2017-05-28 10:44:13 -0700

    Query Parameters and Path vairables.

--- src/main/java/com/melodymoselle/spittr/web/SpittleController.java
+++ src/main/java/com/melodymoselle/spittr/web/SpittleController.java
@@ -4,13 +4,16 @@ import com.melodymoselle.spittr.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
 
+    private static final String MAX_LONG_AS_STRING = Long.toString(Long.MAX_VALUE);
     private SpittleRepository spittleRepository;
 
     @Autowired
@@ -19,10 +22,17 @@ public class SpittleController {
     }
 
     @RequestMapping(method= RequestMethod.GET)
-    public String spittles(Model model) {
+    public String spittles(Model model,
+                           @RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,
+                           @RequestParam(value="count", defaultValue="20") int count) {
         model.addAttribute(
-                spittleRepository.findSpittles(
-                        Long.MAX_VALUE, 20));
+                spittleRepository.findSpittles(Long.MAX_VALUE, 20));
         return "spittles";
+    }
+
+    @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+    public String spittle(@PathVariable long spittleId, Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
     }
 }
